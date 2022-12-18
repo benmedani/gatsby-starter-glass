6bbc3995bb98dc8aa2a63d675b46fe021757638a
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2022-12-18 08:58:54 +0100

    Add: palette color styles

---
+++ src/components/palette-styles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const PaletteStyle = createGlobalStyle`
+--midnight-blue-bg {
+    background-color: #03045eff;
+} 
+
+--star-command-blue-bg {
+    background-color: #0077b6ff;
+}
+--pacific-blue-bg {
+    background-color: #00b4d8ff;
+}
+--middle-blue-bg {
+    background-color: #90e0efff;
+}
+--light-cyan-bg {
+    background-color: #caf0f8ff;
+} 
+
+`;
+
+export default PaletteStyle;
